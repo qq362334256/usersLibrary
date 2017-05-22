a3b10f7ee7021e86d8c2a07f027be9cac4863b0d
miaoyu <362334256@qq.com>
2017-05-23 00:43:00 +0800

    '添加中间件'

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const { json: parJson, urlencoded: parUrlencoded } = require('body-parser');
 const multer = require('multer');
+const compression = require('compression');
+const morgan = require('morgan');
+
 
 // 使用中间件
-app.use(bodyParser.json()); // 解析 application/json 请求方式
-app.use(bodyParser.urlencoded({ extended: true })); // 解析 application/x-www-form-urlencoded 请求方式
+app.use(morgan('tiny')); // 日志中间件
+app.use(compression()); // 压缩数据，这个中间件必须放在最前面
+app.use(parJson()); // 解析 application/json 请求方式
+app.use(parUrlencoded({ extended: true })); // 解析 application/x-www-form-urlencoded 请求方式
 app.use(multer()); // 解析 multipart/form-data 请求方式
 
 
-
+// 设置
 
 
 
