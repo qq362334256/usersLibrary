1a5fa7658e1f0a3db60acf9b8257bacd36a284d8
miaoyu <362334256@qq.com>
2017-05-31 00:14:33 +0800

    '提交绑定mongodb数据库配置'

--- app.js
+++ app.js
@@ -16,6 +16,19 @@ app.use([...middlewares]);
 
 
 
-app.get('/', (req, res) => res.send('get请求'));
+
+app.get('/', (req, res) => {
+	// 连接数据库
+	// const db = require('./DB/mongoDB.js');
+	// db.bind('BD_USERS');
+	// console.log(db.DB_USERS.find());
+
+
+	res.send('get请求');
+});
+
+
+
+
 
 app.listen(port, host, () => console.log('API服务启动，端口号为：5001'));
